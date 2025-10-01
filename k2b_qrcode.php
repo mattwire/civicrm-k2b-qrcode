33e98f8feda3928addda2c61aecf6b235872a4ef
Matthew Wire (MJW) <mjw@mjwconsult.co.uk>
2025-10-01 15:49:11 +0100

    civix upgrade

--- k2b_qrcode.php
+++ k2b_qrcode.php
@@ -13,15 +13,6 @@ function k2b_qrcode_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function k2b_qrcode_civicrm_xmlMenu(&$files) {
-  _k2b_qrcode_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,107 +22,12 @@ function k2b_qrcode_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function k2b_qrcode_civicrm_postInstall() {
-  _k2b_qrcode_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function k2b_qrcode_civicrm_uninstall() {
-  _k2b_qrcode_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function k2b_qrcode_civicrm_enable() {
   _k2b_qrcode_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function k2b_qrcode_civicrm_disable() {
-  _k2b_qrcode_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function k2b_qrcode_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _k2b_qrcode_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function k2b_qrcode_civicrm_managed(&$entities) {
-  _k2b_qrcode_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function k2b_qrcode_civicrm_caseTypes(&$caseTypes) {
-  _k2b_qrcode_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function k2b_qrcode_civicrm_angularModules(&$angularModules) {
-  _k2b_qrcode_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function k2b_qrcode_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _k2b_qrcode_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function k2b_qrcode_civicrm_entityTypes(&$entityTypes) {
-  _k2b_qrcode_civix_civicrm_entityTypes($entityTypes);
 }
 
 function k2b_qrcode_civicrm_qrcodecheckin_tokenValues(&$values, $contact_id, &$handled) {
